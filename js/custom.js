da0bbf627e2903cd658917cb2ea9b8cf4b873823
mvaworld <121222210+mvaworld@users.noreply.github.com>
2023-04-21 21:23:46 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -21,6 +21,10 @@ $("#remove-pop").click(function (event) {
   $("#popup2").toggleClass("open");
 });
 
+$("#dead").click(function (event) {
+  $("#popup2").toggleClass("open");
+});
+
 //----------------------------
 $("#counter").data("count", 2);
 $("#update").click(function () {
@@ -35,3 +39,37 @@ $("#update").click(function () {
     return count;
   });
 });
+
+
+// конечная дата
+const deadline = new Date(Date.now() + 6000999);
+// id таймера
+let timerId = null;
+// склонение числительных
+function declensionNum(num, words) {
+  return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+}
+// вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+function countdownTimer() {
+  const diff = deadline - new Date();
+  if (diff <= 0) {
+    clearInterval(timerId);
+    // alert('Время вышло')
+    popupBg.classList.add('open');
+    // window.location.reload();
+    // location.reload();
+  }
+  const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+  const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+  $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+  $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+  $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+  $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+}
+// получаем элементы, содержащие компоненты даты
+const $minutes = document.querySelector('.timer__minutes');
+const $seconds = document.querySelector('.timer__seconds');
+// вызываем функцию countdownTimer
+countdownTimer();
+// вызываем функцию countdownTimer каждую секунду
+timerId = setInterval(countdownTimer, 1000);
